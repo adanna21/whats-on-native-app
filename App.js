4c0c9d2d700ccea03818a17af12bd5cfa59031b1
Adanna Omatu <adaomatu@gmail.com>
2017-12-15 11:58:34 -0500

    updated app.js

--- App.js
+++ App.js
@@ -1,15 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default class App extends React.Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  fetchShow () {
+    fetch('https://api-staging.trakt.tv/shows/game-of-thrones?extended=full', {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': 'dc60acabe3d5d953393a0e332d8d7d3c5e3c7b7710158b5223526e79f1cf597a'
+      }
+    })
+      .then(res => res.json())
+      .then(res => console.log('res ', res))
+      .catch(err => console.log('error ', err))
+  }
+  getShow () {
+    fetch('http://24.185.162.181/shows', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'token': 'V18A2txYHtwZrjMpZBzVqwBL',
+        'Authentication': 'Token V18A2txYHtwZrjMpZBzVqwBL'
+      },
+      body:  JSON.stringify({
+        "name": "Glee",
+        "trakt_id": "uuuuu",
+        "tvdb_id": "hhhhh",
+        "image_url": "jjjkk"
+      })
+    })
+  }
+  // async fetchShow () {
+  //   try {
+  //     let response = await fetch(
+  //       'https://api-staging.trakt.tv/shows/game-of-thrones?extended=full', {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           'trakt-api-version': '2',
+  //           'trakt-api-key': 'dc60acabe3d5d953393a0e332d8d7d3c5e3c7b7710158b5223526e79f1cf597a'
+  //         }
+  //       })
+  //     let responseJson = await response.json()
+  //     return responseJson.shows
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
+
+  render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>Open App.js to start working on your app!</Text> */}
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text >Shake your phone to open the developer menu.</Text>
+        <Button onPress={this.fetchShow.bind(this) } title='Press Me'>Press Me</Button>
+        <Button onPress={this.getShow.bind(this) } title='Login'>Create</Button>
+        {/* <Button onPress={this.fetchShows.bind(this) } title='Press Me'>Get Shows</Button> */}
       </View>
-    );
+    )
   }
 }
 
@@ -18,6 +73,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
