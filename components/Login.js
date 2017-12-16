8262584dc67ba08b594dda19b45f8264ace02caa
Adanna Omatu <adaomatu@gmail.com>
2017-12-16 15:00:28 -0500

    added info to login

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View} from 'react-native'
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native'
 
 export default class Login extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      username: '',
+      password: '',
+      errors: ''
+    }
+  }
+
+  async onLogin () {
+    try {
+      let response = await fetch('https://agile-forest-84610.herokuapp.com/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          session: {
+            username: this.state.username,
+            password: this.state.password
+          }
+        })
+      })
+      let res = await response.text()
+      if (response.status >= 200 && response.status < 300) {
+        console.log('res is:' + res)
+      } else {
+        let errors = res
+        throw errors
+      }
+    } catch (errors) {
+      console.log('errors are:' + errors)
+    }
+  }
+
   render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>What's On!?</Text>
+        <TextInput
+          placeholder='username'
+          onChangeText={(val) => this.setState({username: val})}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder='password'
+          onChangeText={(val) => this.setState({password: val})}
+          // secureTextEntry={true}
+          style={styles.input}
+        />
+        <TouchableOpacity onPress={this.onRegister.bind(this)}>
+          <Text>Login</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -18,8 +67,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  title: {
-    fontSize: 40,
+  input: {
+    fontSize: 30,
     color: '#fff'
   }
 })
