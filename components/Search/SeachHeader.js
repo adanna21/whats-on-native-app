eaafb3e43335d306f4a387c4b4c3cedb8e84cc31
Adanna Omatu <adaomatu@gmail.com>
2017-12-16 22:53:22 -0500

    added content to search header

--- components/Search/SeachHeader.js
+++ components/Search/SeachHeader.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View} from 'react-native'
+import {Header, Item, Icon, Input} from 'native-base'
 
 export default class SearchHeader extends Component {
   render () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>What's On!?</Text>
-      </View>
+      <Header
+        style={height: 80}
+        searchBar
+        rounder
+      >
+        <Item>
+          <Icon name='ios-search'/>
+          <Input/>
+        </Item>
+      </Header>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3498db',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: 40,
-    color: '#fff'
-  }
-})
